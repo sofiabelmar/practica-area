cf791ea053ff5da1956d37f80c4b32a14bcd5b89
Sofía Beltrán Martín <sofiabelmar@outlook.com>
2018-09-27 08:47:46 -0700

    Agregamos controles de usuario y combo box

--- PracticaAreas/MainWindow.xaml.cs
+++ PracticaAreas/MainWindow.xaml.cs
@@ -28,45 +28,93 @@ namespace PracticaAreas
 
         private void btn1_Click(object sender, RoutedEventArgs e)
         {
-            float baser = float.Parse(baserect.Text);
+            /*float baser = float.Parse(baserect.Text);
             float alturar = float.Parse(alturarect.Text);
 
             float arect = baser * alturar;
 
-            recttotal.Text = arect.ToString();
+            recttotal.Text = arect.ToString();*/
 
         }
 
         private void bnt2_Click(object sender, RoutedEventArgs e)
         {
-            float trbase = float.Parse(basetr.Text);
+            /*float trbase = float.Parse(basetr.Text);
             float traltura = float.Parse(altr.Text);
 
             float atr = (trbase * traltura) / 2;
 
-            trtotal.Text = atr.ToString();
+            trtotal.Text = atr.ToString();*/
 
         }
 
         private void btn3_Click(object sender, RoutedEventArgs e)
         {
-            float radio = float.Parse(radcir.Text);
+           /* float radio = float.Parse(radcir.Text);
 
             float acir = 3.14160f * (radio * radio);
 
-            cirtotal.Text = acir.ToString();
+            cirtotal.Text = acir.ToString();*/
         }
 
         private void btn4_Click(object sender, RoutedEventArgs e)
         {
-            float menor = float.Parse(bmenor.Text);
+            /*float menor = float.Parse(bmenor.Text);
             float mayor = float.Parse(bmayor.Text);
             float altura = float.Parse(alturatrap.Text);
 
             float atrap = altura * ((menor + mayor) / 2);
 
-            areatrap.Text = atrap.ToString();
+            areatrap.Text = atrap.ToString();*/
 
+        }
+
+        private void cb_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            PanelConfiguracion.Children.Clear();
+
+            switch (cb.SelectedIndex)
+            {
+                case 0: //rectangulo
+                    PanelConfiguracion.Children.Add(new ControlAreaRectangulo());
+                    break;
+                case 1: //triangulo
+                    PanelConfiguracion.Children.Add(new ControlAreaTriangulo());
+                    break;
+                case 2:
+                    PanelConfiguracion.Children.Add(new ControlAreaRectangulo());
+                    break;
+                case 3:
+                    PanelConfiguracion.Children.Add(new ControlAreaCirculo());
+                    break;
+                case 4:
+                    PanelConfiguracion.Children.Add(new ControlAreaTrapecio());
+                    break;
+                default:
+                    break;
+            }
+
+        }
+
+        private void btn1_Click_1(object sender, RoutedEventArgs e)
+        {
+
+            double area = 0.0;
+            switch (cb.SelectedIndex)
+
+            {
+                case 0: //cuadrado
+                    break;
+                case 1: //triangulo
+                    break;
+                case 2: //rectangulo
+                    break;
+                case 3:  //circulo
+                    break;
+                case 4: //trapecio
+                    break;
+            }
+            lblResultadoArea.Text = area.ToString();
         }
     }
 }
